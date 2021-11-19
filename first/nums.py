da4b0a386a1ef085f763d59c456ce3a82b0ef4ac
Yauheni Ivashkevich <zheklex@ya.ru>
2021-11-19 16:25:16 +0300

    Added string.py, del text

--- first/nums.py
+++ first/nums.py
@@ -2,14 +2,24 @@ def plus(a, b):
     return a + b
 
 
-text = "new text"
-
-
 def minus(a, b):
     return a - b
 
 
+def nul(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(4, 2))
+    print(nul(3, 3))
+    print(minus(4, 2))
+    print(minus(4, 2))
+    print(minus(4, 2))
+    print(minus(4, 2))
+    print(minus(4, 2))
+    print(minus(4, 2))
+    print(minus(4, 2))
     print(minus(4, 2))
     print(minus(4, 2))
